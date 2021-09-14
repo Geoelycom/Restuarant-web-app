8ac64943a7139a8c044722ee37b04c2191147443
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-09-14 16:28:56 +0100

    Made an algorithem to reduce words of recipes

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -7,6 +7,22 @@ export const clearInput = () => {
 export const clearResult = () => {
 	elements.searchResultList.innerHTML = '';
 }
+
+const limitRecipeTitle = (title, limit = 17) => {
+	const newTitle = [];
+	if (title.length > limit) {
+		title.split(' ').reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur)
+			}
+			return acc + cur.length;
+		}, 0)
+		// return result of the acc
+		return `${newTitle.join(' ')}...`
+	}
+	return title;
+}
+
 const renderRecipes = recipe => {
 	let markup = '';
 	markup += `
@@ -16,7 +32,7 @@ const renderRecipes = recipe => {
 																	<img src="${recipe.image_url}" alt="${recipe.title}">
 													</figure>
 													<div class="results__data">
-																	<h4 class="results__name">${recipe.title}</h4>
+																	<h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
 																	<p class="results__author">${recipe.publisher}</p>
 													</div>
 									</a>
