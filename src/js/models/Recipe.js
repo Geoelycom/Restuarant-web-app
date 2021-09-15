fa74cee4b2922228daa94d5898924df792750e54
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-09-15 19:38:46 +0100

    Create recipe component

---
+++ src/js/models/Recipe.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export default class Recipe {
+	constructor(id) {
+		this.id = id;
+	}
+
+	async fetchRecipeId() {
+		try {
+			const recipeId = await axios('https://forkify-api.herokuapp.com/api/get', {
+				params: {
+					rId: this.id
+				}
+			});
+			// this.title = recipeId.data.recipe.title;
+			// this.author = recipeId.data.recipe.publisher;
+			// this.img = recipeId.data.recipe.img_url;
+			// this.url = recipeId.data.recipe.source_url;
+			// this.ingredients = recipeId.data.recipe.ingredients;
+			console.log(recipeId)
+			let { title, image_url, source_url, ingredients, publisher } = recipeId.data.recipe;
+			Object.assign(this, { title, image_url, source_url, ingredients, publisher })
+			//console.log(title)
+		} catch (error) {
+			console.log('wrong response data')
+		}
+	}
+}
