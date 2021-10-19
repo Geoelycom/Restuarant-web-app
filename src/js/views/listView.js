8a99fb538c8c11a3fc83d9b71398dcb6960fda56
Elyan <ekenimohelyanpheta@gmail.com>
2021-10-19 16:26:04 -0700

    create list view

---
+++ src/js/views/listView.js
@@ -0,0 +1,30 @@
+import {
+  elements
+} from "./base";
+
+export const renderShoppingListItem = item => {
+  const shoppingMarkUp = `
+  <li class="shopping__item" data-itemid=${item.id}>
+        <div class="shopping__count">
+            <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
+                <p>${item.unit}</p>
+        </div>
+            <p class="shopping__description">${item.ingredient}</p>
+              <button class="shopping__delete btn-tiny">
+                  <svg>
+                    <use href="img/icons.svg#icon-circle-with-cross"></use>
+                  </svg>
+              </button>
+    </li>
+  
+  `;
+  elements.shoppingRecipeList.insertAdjacentHTML('beforeend', shoppingMarkUp);
+
+};
+
+
+export const deleteShoppingItem = id => {
+  const item = document.querySelector(`[dataitem-id="${id}"]`);
+  if(item)
+  item.parentElement.removeChild(item);
+};
